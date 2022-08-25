b65bdc368a7352724b2e2bc9559d5019231c0ce6
gechcom <getinetay@gmail.com>
2022-08-25 17:36:21 +0300

    Update variable

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-from variable_load_5 import a
-
-
-def main():
-    print(a)
-
 if __name__ == "__main__":
-    main()
+    """print load variable"""
+    from variable_load_5 import a
+    print("{:d}".format(a))
