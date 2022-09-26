5bf5537341f67a401b8f2e4806acdb1917847cc0
gechcom <getinetay@gmail.com>
2022-09-26 21:21:30 +0300

    geometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is the BaseGeometry module.
+
+The BaseGeometry module defines the BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """Represents a BaseGeometry."""
+    pass
