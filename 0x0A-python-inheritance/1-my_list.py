6b07f4119a5685b7899c15051954aef218f7257c
gechcom <getinetay@gmail.com>
2022-09-26 21:14:02 +0300

    my list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This is the "my list" module.
+The my list module supplies one function, print_sorted().
+"""
+
+
+class MyList(list):
+    """Represents a MyList."""
+
+    def print_sorted(self):
+        """Print the list in ascending order."""
+        print(sorted(self))
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
