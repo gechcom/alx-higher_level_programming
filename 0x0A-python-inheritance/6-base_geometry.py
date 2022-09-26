4972185104ae32776795caf9d18c8f68efbde65b
gechcom <getinetay@gmail.com>
2022-09-26 21:22:23 +0300

    geo

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is the BaseGeometry module.
+
+The BaseGeometry module contains one function, area().
+"""
+
+
+class BaseGeometry:
+    """Represents a BaseGeometry."""
+
+    def area(self):
+        """Raise an exception."""
+        raise Exception('area() is not implemented')
